92037e1d7a98d52d89e9f6ba88a69d3d1401457e
unknown <andrecampos.webdesign@gmail.com>
2013-10-18 21:41:28 -0300

    pasta de testes e jquery funcionando

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,17 @@
 
 
-var home = ('.home')
 
-$( "home" ).click(function() {
-  	$( ".areas-template" ).show( "slow" );
+
+
+$(document).ready(function() {
+  	$( ".banner, .especialies" ).fadeIn( 2000 );
 });
 
 
+$( "a" ).click(function() {
+  	$( ".banner" ).hide( 1000 );
+});
 
+$( "a" ).hover(function() {
+  	$( ".areas" ).hide( 1000 );
+});
